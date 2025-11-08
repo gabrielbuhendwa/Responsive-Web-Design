1a0c24ca600c8548e9759a4cc032b48f05719269
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-08 05:28:15 +0200

    add this to my repository

--- script/main.js
+++ script/main.js
@@ -1,14 +1,34 @@
-// Mobile menu toggle
-        const mobileMenuBtn = document.getElementById('mobile-menu-btn');
-        const navMenu = document.getElementById('nav-menu');
+//working with mobile menu toggle
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+const navMenu = document.getElementById('nav-menu');
 
-        mobileMenuBtn.addEventListener('click', () => {
-            navMenu.classList.toggle('active');
-        });
+mobileMenuBtn.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+});
+// Close mobile menu when clicking on a link
+document.querySelectorAll('#nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+    });
+});
 
-        // Close mobile menu when clicking on a link
-        document.querySelectorAll('#nav-menu a').forEach(link => {
-            link.addEventListener('click', () => {
-                navMenu.classList.remove('active');
-            });
-        });
+
+
+
+//Scroll to top button
+const scrollTopBtn = document.getElementById('scroll-top');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+            scrollTopBtn.classList.add('active');
+    }
+    else{
+        scrollTopBtn.classList.remove('active');
+    }
+});
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
